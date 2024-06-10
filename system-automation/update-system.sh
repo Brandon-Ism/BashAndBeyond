d4554837269c09827921bd1d2045ae23e16dede3
Brandon-Ism <brandonismalej@gmail.com>
2024-06-09 19:24:52 -0700

    Add 'dist-upgrade'
    Add to documentation for further comprehension

--- system-automation/update-system.sh
+++ system-automation/update-system.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# The above line tells the system that the script should be run using Bash shell. 
 
 # This script is mean to update and upgrade the system, as well as cleanup any unnecessary packages that are no longer needed.
 
@@ -6,15 +7,17 @@
 
 update_sys() {
     echo "Updating package lists..."
-    sudo apt-get update
+    sudo apt-get update 
 
     echo "Upgrading packages..."
-    sudo apt-get upgrade -y
+    sudo apt-get upgrade -y # Upgrade all packages to latest versions.
 
+    echo "Performing full distribution upgrade..."
+    sudo apt-get dist-upgrade -y # Full upgrade of system, may install/remove oackages as necessary.
 
     echo "Cleaning up unnecessary packages..."
-    sudo apt-get autoremove -y
-    sudo apt-get autoclean -y
+    sudo apt-get autoremove -y # Removes packages that were installed as dependencies for other packages, but no longer needed.
+    sudo apt-get autoclean -y # Cleans the local repository of retrived package files. 
 
     echo "System update, upgrade, and cleaning complete!"
 }
